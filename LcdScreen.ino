61c205077ec250fa142230372bcfd73600d8921f
gyurci08 <93055206+gyurci08@users.noreply.github.com>
2022-05-25 09:12:36 +0200

    asd

--- LcdScreen.ino
+++ LcdScreen.ino
@@ -1,21 +1,22 @@
 
 #include <LiquidCrystal.h>
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 0, en = 1, d4 = 13, d5 = 12, d6 = 11, d7 = 10;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   
-  // Print a message to the LCD.
-  lcd.setCursor(0,0);
-  lcd.print("Asd");
-  
-  lcd.setCursor(0,1);
-  lcd.print("2");
+
 }
 
 void loop() {
+    // Print a message to the LCD.
+  lcd.setCursor(0,0);
+  lcd.print("12");
   
+  lcd.setCursor(0,1);
+  lcd.print("2");
 }
